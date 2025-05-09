12bd386a9d60bea5013538d21340c91d9d642e49
Seunghyuk Oh <seunghyukoh0@gmail.com>
2025-05-09 13:02:52 +0900

    refactor: Add __init__.py

--- revise/__init__.py
+++ revise/__init__.py
@@ -5,9 +5,25 @@ from .generators import (
     VllmGenerator,
 )
 
+from .evaluators import (
+    BaseComparisonEvaluator,
+    GSM8KEvaluator,
+    MATHEvaluator,
+)
+
+from .utils import configure_logging, hash_params
+
 __all__ = [
+    # .generators
     "BaseGenerator",
     "GenerationParams",
     "VllmGenerator",
     "VllmGenerationParams",
+    # .evaluators
+    "BaseComparisonEvaluator",
+    "GSM8KEvaluator",
+    "MATHEvaluator",
+    # .utils
+    "configure_logging",
+    "hash_params",
 ]
---
+++ revise/evaluators/__init__.py
@@ -0,0 +1,7 @@
+from .comparison_evaluator import BaseComparisonEvaluator, GSM8KEvaluator, MATHEvaluator
+
+__all__ = [
+    "BaseComparisonEvaluator",
+    "GSM8KEvaluator",
+    "MATHEvaluator",
+]
