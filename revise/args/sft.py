efdc2686799e44a0b0b4ede40dadd5616b946806
Seunghyuk Oh <seunghyukoh0@gmail.com>
2025-06-19 16:02:55 +0900

    refactor: Refactor supervised fine-tuning script using trl library

--- revise/args/sft.py
+++ revise/args/sft.py
@@ -1,93 +1,31 @@
 from dataclasses import dataclass, field
-from typing import List, Optional
-from transformers import TrainingArguments as HfTrainingArguments
 
-from revise.args.parser import BaseArguments
+from trl import SFTConfig as DefaultSFTConfig
 
 
 @dataclass
-class ModelArguments:
+class SFTConfig(DefaultSFTConfig):
     model_name_or_path: str = field(
-        metadata={"help": "The model name or path to use for training"}
+        default=None, metadata={"help": "The model name or path to use for training"}
     )
-
-    tokenizer_name_or_path: Optional[str] = field(
+    tokenizer_name_or_path: str = field(
         default=None,
         metadata={"help": "The tokenizer name or path to use for training"},
     )
-
-    chat_template: Optional[str] = field(
-        default=None,
-        metadata={"help": "The template for the chat."},
+    dataset_path: str = field(
+        default=None, metadata={"help": "The dataset path to use for training"}
+    )
+    dataset_name: str = field(
+        default=None, metadata={"help": "The dataset name to use for training"}
     )
 
-    device_map: Optional[str] = field(
-        default=None,
-        metadata={"help": "The device to use for training"},
-    )
-
-    dtype: Optional[str] = field(
-        default=None,
-        metadata={"help": "The dtype to use for training"},
-    )
-
-    # LoRA (Low Rank Adaptation)
-    use_lora: Optional[bool] = field(
-        default=False,
-        metadata={
-            "help": "Use Low Rank Adaptation",
-        },
-    )
-    lora_rank: Optional[int] = field(
-        default=16,
-        metadata={
-            "help": "Set the rank for Low Rank Adaptation",
-        },
-    )
-    lora_alpha: Optional[float] = field(
-        default=4,
-        metadata={
-            "help": "Set the alpha for Low Rank Adaptation",
-        },
-    )
-    lora_dropout: Optional[float] = field(
-        default=0.1,
-        metadata={
-            "help": "Set the dropout for Low Rank Adaptation",
-        },
-    )
-
-
-@dataclass
-class TrainingArguments(HfTrainingArguments):
-    pass
-
-
-class SFTArguments(BaseArguments):
-    ARG_COMPONENTS = [
-        ModelArguments,
-        TrainingArguments,
-    ]
-
-    model_args: ModelArguments
-    training_args: TrainingArguments
-
-    def __init__(
-        self,
-        model_args: ModelArguments,
-        training_args: TrainingArguments,
-    ):
-        super().__init__()
-
-        assert model_args is not None
-        assert training_args is not None
-
-        self.model_args = model_args
-        self.training_args = training_args
-
-    def to_dict(self):
-        return {
-            "uuid": self.uuid,
-            **self.model_args.__dict__,
-            **self.training_args.__dict__,
-        }
+    def __post_init__(self):
+        super().__post_init__()
+        if self.model_name_or_path is None:
+            raise ValueError("model_name_or_path is required")
+        if self.tokenizer_name_or_path is None:
+            raise ValueError("tokenizer_name_or_path is required")
+        if self.dataset_path is None:
+            raise ValueError("dataset_path is required")
+        if self.dataset_name is None:
+            raise ValueError("dataset_name is required")
--- revise/sft.py
+++ revise/sft.py
@@ -1,178 +1,64 @@
-import logging
-import os
-from pprint import pprint
-from typing import Tuple
-
 import torch
-import wandb
-from accelerate import Accelerator
-from datasets import load_dataset
-from dotenv import load_dotenv
-from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer,
-    PreTrainedModel,
-    PreTrainedTokenizer,
-    Trainer,
-    set_seed,
-)
-from trl import DataCollatorForCompletionOnlyLM
+from transformers import AutoModelForCausalLM, AutoTokenizer, HfArgumentParser
+from trl import SFTTrainer
 
-from revise.args.parser import parse_args
-from revise.args.sft import ModelArguments, SFTArguments
-
-logger = logging.getLogger(__name__)
-is_main_process = True
+from revise.args.sft import SFTConfig
 
 
-def get_model_and_tokenizer(
-    model_args: ModelArguments,
-) -> Tuple[PreTrainedModel, PreTrainedTokenizer]:
-    device_map = model_args.device_map
-    if device_map is None:
-        device_map = (
-            "cuda"
-            if torch.cuda.is_available()
-            else "mps"
-            if torch.backends.mps.is_available()
-            else "cpu"
-        )
+def load_dataset(path: str, name: str):
+    from datasets import load_dataset
 
-    dtype = getattr(torch, model_args.dtype) if model_args.dtype is not None else None
-
-    model = AutoModelForCausalLM.from_pretrained(
-        model_args.model_name_or_path,
-        device_map=device_map,
-        use_cache=False,
-        torch_dtype=dtype,
-        attn_implementation="flash_attention_2" if device_map == "cuda" else None,
-    )
-
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_args.tokenizer_name_or_path or model_args.model_name_or_path,
-    )
-
-    if tokenizer.pad_token is None:
-        tokenizer.pad_token = tokenizer.eos_token
-
-    if model_args.chat_template is not None:
-        tokenizer.chat_template = model_args.chat_template
-
-    return model, tokenizer
-
-
-def get_dataset(tokenizer):
-    # TODO: Update this to your own dataset.
-    dataset = load_dataset("openai/gsm8k", name="main")
-
-    def routine(sample):
-        question = sample["question"]
-        answer = sample["answer"]
-
-        from revise.prompts import prepare_gsm8k_chat_messages
-
-        messages = prepare_gsm8k_chat_messages(question)
-        messages += [{"role": "assistant", "content": answer}]
-
-        encoding = tokenizer.apply_chat_template(
-            messages,
-            tokenize=True,
-            return_tensors="pt",
-            return_dict=True,
-            return_assistant_tokens_mask=True,
-        )
-
-        return {
-            "input_ids": encoding["input_ids"].squeeze(),
-            "assistant_masks": encoding["assistant_masks"].squeeze(),
+    dataset = load_dataset(path, name=name)
+    dataset = dataset.map(
+        lambda x: {
+            "prompt": f"{x['question']}\nThink step by step and then answer.",
+            "completion": x["answer"],
         }
-
-    dataset = dataset.map(routine, batched=False)
-    train_dataset = dataset["train"]
-    eval_dataset = dataset["test"]
-
-    return train_dataset, eval_dataset
-
-
-def main(args: SFTArguments):
-    model_args = args.model_args
-    training_args = args.training_args
-
-    # Set seed
-    set_seed(training_args.seed)
-
-    # Load tokenizer and model
-    with Accelerator().main_process_first():
-        model, tokenizer = get_model_and_tokenizer(model_args)
-
-    # Log model params
-    model_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    logger.info(f"trainable model_params: {model_params}")
-
-    # Log model
-    if is_main_process:
-        pprint(model)
-
-    # Load dataset
-    with Accelerator().main_process_first():
-        train_dataset, validation_dataset = get_dataset(tokenizer=tokenizer)
-
-    trainer = Trainer(
-        model,
-        args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=validation_dataset,
-        processing_class=tokenizer,
-        data_collator=DataCollatorForCompletionOnlyLM(
-            response_template="<|assistant|>\n",
-            tokenizer=tokenizer,
-            pad_to_multiple_of=8,
-        ),
     )
 
-    # Training
-    train_result = trainer.train()
-    trainer.save_model()  # Saves the tokenizer too for easy upload
-
-    metrics = train_result.metrics
-
-    metrics["train_samples"] = len(train_dataset)
-
-    trainer.log_metrics("train", metrics)
-    trainer.save_metrics("train", metrics)
-    trainer.save_state()
+    return {"train": dataset["train"], "eval": dataset["test"]}
 
 
 if __name__ == "__main__":
-    import torch._dynamo as d2
+    parser = HfArgumentParser(SFTConfig)
+    [training_args] = parser.parse_args_into_dataclasses()
 
-    d2.config.capture_scalar_outputs = True
+    dataset = load_dataset(training_args.dataset_path, training_args.dataset_name)
 
-    load_dotenv()
+    model = AutoModelForCausalLM.from_pretrained(
+        training_args.model_name_or_path,
+        attn_implementation="flash_attention_2",  # Enable Flash Attention
+        torch_dtype="bfloat16",
+        device_map="auto",
+    )
 
-    logger.setLevel(logging.INFO)
-    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+    tokenizer = AutoTokenizer.from_pretrained(
+        training_args.tokenizer_name_or_path or training_args.model_name_or_path
+    )
 
-    # Parse arguments
-    args: SFTArguments = parse_args(SFTArguments)
+    trainer = SFTTrainer(
+        model=model,
+        processing_class=tokenizer,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["eval"] if "eval" in dataset else None,
+        args=training_args,
+    )
 
-    accelerator = Accelerator()
+    # Clear cache to mitigate memory issues
+    torch.cuda.empty_cache()
 
-    is_main_process = accelerator.is_main_process
-    if is_main_process:
-        raw_args = args.to_dict()
+    # Train the model
+    train_result = trainer.train()
 
-        # Print arguments
-        pprint(raw_args)
+    # Save the model
+    trainer.save_model()
 
-        wandb.init(
-            id=args.uuid,
-            project=os.getenv("WANDB_PROJECT", "revise"),
-            name=args.training_args.run_name,
-            config=raw_args,
-            group="sft",
-        )
+    # Save the metrics of the training
+    if hasattr(train_result, "metrics"):
+        metrics = train_result.metrics
+        metrics["train_samples"] = len(dataset["train"])
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
 
-    accelerator.wait_for_everyone()
-
-    main(args)
+    # Save the state of the trainer
+    trainer.save_state()
