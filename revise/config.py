5593b8a017a6d8788b336566f4d9fd3168a785d5
Seunghyuk Oh <seunghyukoh0@gmail.com>
2025-06-20 09:49:31 +0900

    docs: Add comment for DEFAULT_REFINE_TOKEN to clarify usage for different models

--- revise/config.py
+++ revise/config.py
@@ -1,3 +1,4 @@
 DEFAULT_CHAT_TEMPLATE = "{% for message in messages %}{% if message['role'] == 'system' %}{{ '<|system|>\n' + message['content'] + '\n' }}{% elif message['role'] == 'user' %}{{ '<|user|>\n' + message['content'] + '\n' }}{% elif message['role'] == 'assistant' %}{% if not loop.last %}{{ '<|assistant|>\n'  + message['content'] + eos_token + '\n' }}{% else %}{{ '<|assistant|>\n'  + message['content'] + eos_token }}{% endif %}{% endif %}{% if loop.last and add_generation_prompt %}{{ '<|assistant|>\n' }}{% endif %}{% endfor %}"
 
 DEFAULT_REFINE_TOKEN = "<|reserved_special_token_0|>"  # Llama 3
+# DEFAULT_REFINE_TOKEN = "<|refine|>"  # For any other model. Add this to the tokenizer as a special token.
