48608db9d16eddf7b14ace1fd3fa3bfb46bc3e9b
Seunghyuk Oh <seunghyukoh0@gmail.com>
2025-07-03 14:08:12 +0900

    refactor: Update SFTConfig to use Optional types for tokenizer_name_or_path and dataset_name

--- revise/args/sft.py
+++ revise/args/sft.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from typing import Optional
 
 from trl import SFTConfig as DefaultSFTConfig
 
@@ -8,23 +9,21 @@ class SFTConfig(DefaultSFTConfig):
     model_name_or_path: str = field(
         default=None, metadata={"help": "The model name or path to use for training"}
     )
-    tokenizer_name_or_path: str = field(
+    tokenizer_name_or_path: Optional[str] = field(
         default=None,
         metadata={"help": "The tokenizer name or path to use for training"},
     )
     dataset_path: str = field(
         default=None, metadata={"help": "The dataset path to use for training"}
     )
-    dataset_name: str = field(
-        default=None, metadata={"help": "The dataset name to use for training"}
+    dataset_name: Optional[str] = field(
+        default="default", metadata={"help": "The dataset name to use for training"}
     )
 
     def __post_init__(self):
         super().__post_init__()
         if self.model_name_or_path is None:
             raise ValueError("model_name_or_path is required")
-        if self.tokenizer_name_or_path is None:
-            raise ValueError("tokenizer_name_or_path is required")
         if self.dataset_path is None:
             raise ValueError("dataset_path is required")
         if self.dataset_name is None:
